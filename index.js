3f24874518e7e646fc5e77fa0e95daeacea7e2e9
Satyen-Sen <76486273+Satyen-Sen@users.noreply.github.com>
2022-03-14 02:40:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// Detecting Mouse Click
+document.querySelector(".btn").addEventListener("click", mouseClick);
+
+function mouseClick() {
+        gamePlay();
+        buttonAnimation();
+}
+
+function gamePlay() {
+    var randomNumber1 = Math.floor((Math.random()*6)+1);
+    var randomDice1 = "images/dice" + randomNumber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
+
+    var randomNumber2 = Math.floor((Math.random()*6)+1);
+    var randomDice2 = "images/dice" + randomNumber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins";
+    } else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+    } else {
+        document.querySelector("h1").innerHTML = "This is a Draw";
+    }
+}
+
+function buttonAnimation() {
+    document.querySelector(".btn").classList.add("pressed");
+    setTimeout(function name() {document.querySelector(".btn").classList.remove("pressed");}, 150);
+  }
